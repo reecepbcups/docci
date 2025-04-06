6a0b796707542fe3368167a1376272136c888e07
Reece Williams <reecepbcups@gmail.com>
2025-04-06 17:15:31 -0500

    info

--- src/config.py
+++ src/config.py
@@ -80,7 +80,7 @@ class Config:
     def from_json(cls, json: Dict) -> "Config":
         c = Config(json['paths'], json.get('env_vars', {}), json.get('cleanup_cmds', []), json.get('pre_cmds', []))
         c.working_dir = json.get('working_dir', None)
-        c.log_level = json.get('log_level', "ERROR")
+        c.log_level = json.get('log_level', "INFO")
         return c
 
     def to_json(self):
